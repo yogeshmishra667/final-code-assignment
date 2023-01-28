453d9e3218ca11f3ae8b2ff946e22de383cc23d1
yogesh mishra <yogeshmishra667@gmail.com>
2023-01-28 15:00:38 +0530

    fix error

--- read-fs.js
+++ read-fs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 fs.readdir('.', (err, files) => { // '.' is the current directory
-  if (err) throw err;image.png
+  if (err) throw err
   for (const file of files) {
     console.log(file);
   }
